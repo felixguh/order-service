2c5e85c880619cc7422ed72da9b8109f9526165d
Gustavo <gustavo.santosfelix@gmail.com>
2020-04-25 14:09:07 -0300

    feat(exception): create handler

---
+++ src/main/java/br/com/orderservice/exception/handler/ExceptionHandlerController.java
@@ -0,0 +1,8 @@
+package br.com.orderservice.exception.handler;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandlerController {
+
+}
