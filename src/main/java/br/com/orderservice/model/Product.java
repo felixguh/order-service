666cd5b5f56a67d49a208271825a0f821f89cf42
Gustavo <gustavo.santosfelix@gmail.com>
2020-04-27 20:40:44 -0300

    feat(model): create product document

---
+++ src/main/java/br/com/orderservice/model/Product.java
@@ -0,0 +1,18 @@
+package br.com.orderservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+	private Long productNumber;
+	
+	private Integer amount;
+
+}
