ebb01cce2b9fba9027b933cd4cf64d92368999c7
Gustavo <gustavo.santosfelix@gmail.com>
2020-04-27 20:41:11 -0300

    feat(payload): create product payload

---
+++ src/main/java/br/com/orderservice/model/payload/ProductPayload.java
@@ -0,0 +1,21 @@
+package br.com.orderservice.model.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode
+public class ProductPayload {
+
+	@EqualsAndHashCode.Include
+	private Long productNumber;
+
+	@EqualsAndHashCode.Exclude
+	private Integer amount;
+}
