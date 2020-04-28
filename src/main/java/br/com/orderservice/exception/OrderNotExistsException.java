b31924ec304378f269524c5d291013f791ac6326
Gustavo <gustavo.santosfelix@gmail.com>
2020-04-27 22:38:46 -0300

    feat(exception): create exception to validate a not found order

---
+++ src/main/java/br/com/orderservice/exception/OrderNotExistsException.java
@@ -0,0 +1,11 @@
+package br.com.orderservice.exception;
+
+public class OrderNotExistsException extends RuntimeException {
+
+	private static final long serialVersionUID = 7946921705290426765L;
+
+	public OrderNotExistsException() {
+		super();
+	}
+
+}
