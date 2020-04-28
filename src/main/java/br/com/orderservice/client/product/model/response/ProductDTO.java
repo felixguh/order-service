a85f2a74f780c2a5b0020c24c4641bed3babd814
Gustavo <gustavo.santosfelix@gmail.com>
2020-04-27 23:46:46 -0300

    feat(dto): adding annotation builder

--- src/main/java/br/com/orderservice/client/product/model/response/ProductDTO.java
+++ src/main/java/br/com/orderservice/client/product/model/response/ProductDTO.java
@@ -4,10 +4,12 @@ import java.math.BigDecimal;
 
 import br.com.orderservice.client.product.model.enums.Category;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductDTO {
